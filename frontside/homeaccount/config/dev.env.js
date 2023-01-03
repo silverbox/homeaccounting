63b52bd049828f0af5f0687195dc42ea4bd8e875
silverbox <tanaka.eiji@nifty.com>
2023-01-03 11:11:19 +0900

    update module version

--- frontside/homeaccount/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_ENDPOINT_BASE: '"https://fugafuga.execute-api.ap-northeast-1.amazonaws.com/dev/api/"',
-  API_KEY: '"hogehoge"'
-})
--- frontside/homeaccount/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  API_ENDPOINT_BASE: '"/api/"',
-  API_KEY: '""'
-}
